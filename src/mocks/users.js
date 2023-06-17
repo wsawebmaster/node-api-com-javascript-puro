4c30bb40af89e6b073846ec5bc38c6ebbc71a89e
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-06-17 18:05:05 -0300

    API - Header, Query e Parâmetros

---
+++ src/mocks/users.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Wagner',
+  },
+  {
+    id: 2,
+    name: 'Adriana',
+  },
+  {
+    id: 3,
+    name: 'Esther',
+  },
+  {
+    id: 4,
+    name: 'Isac',
+  },
+  {
+    id: 5,
+    name: 'Rafael',
+  },
+  {
+    id: 6,
+    name: 'Arthur',
+  },
+]
